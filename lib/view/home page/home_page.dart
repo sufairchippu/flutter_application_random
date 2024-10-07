1017ed3096c5671b8865fa952ea364eb2e7ebc11
sufairchippu <sufaircippu84@gmail.com>
2024-10-07 17:00:42 +0530

    newww

--- lib/view/home page/home_page.dart
+++ lib/view/home page/home_page.dart
@@ -16,7 +16,6 @@ class _HomePageState extends State<HomePage> {
     'assets/images/d3.png',
     'assets/images/d4.png',
     'assets/images/d5.png',
-    'assets/images/d6.png'
   ];
   int? rnadomnumber;
   @override
